e29daefb6eb3494559b4deddcb15ad2da20e5555
Muukii <muukii.app@gmail.com>
2024-12-02 01:30:55 +0900

    Init

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "ObservationContinuousTracking",
+  platforms: [.iOS(.v17)],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "ObservationContinuousTracking",
+      targets: ["ObservationContinuousTracking"])
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "ObservationContinuousTracking"),
+    .testTarget(
+      name: "ObservationContinuousTrackingTests",
+      dependencies: ["ObservationContinuousTracking"]
+    ),
+  ],
+  swiftLanguageVersions: [.v6]
+)
---
+++ Sources/ObservationContinuousTracking/ObservationContinuousTracking.swift
@@ -0,0 +1,156 @@
+
+import Observation
+import SwiftUI
+import os.lock
+
+struct BookObservation: View, PreviewProvider {
+  var body: some View {
+    ContentView()
+  }
+  
+  static var previews: some View {
+    Self()
+      .previewDisplayName(nil)
+  }
+  
+  private struct ContentView: View {
+    
+    let controller = Controller()
+    
+    var body: some View {
+      Button("Up") {
+        controller.increment()
+      }
+      Button("Background Up") {
+        controller.backgroundIncrement()
+      }
+      .onAppear {
+        
+        withObservationContinuousTracking {
+          _ = controller.count
+        } onChange: {
+          //          MainActor.assumeIsolated {
+          print("Count: \(controller.count)")
+          //          }
+        }
+        
+      }
+    }
+  }
+  
+  @Observable
+  final class Controller: @unchecked Sendable {
+    
+    var count: Int = 0
+    
+    init() {
+      
+    }
+    
+    nonisolated func increment() {
+      count += 1
+    }
+    
+    func backgroundIncrement() {
+      Task {
+        count += 1
+      }
+    }
+    
+  }
+}
+
+private func _a() {
+  withObservationContinuousTracking {
+    //    UI()
+  } onChange: {
+    
+  }
+}
+
+@MainActor
+func UI() {
+  
+}
+
+public struct ObservationTrackingSubscription: Sendable {
+  
+  private let onCancel: @Sendable () -> Void
+  
+  init(onCancel: @escaping @Sendable () -> Void) {
+    self.onCancel = onCancel
+  }
+  
+  public func cancel() {
+    onCancel()
+  }
+  
+}
+
+public func withObservationContinuousTracking(
+  applying block: @escaping () -> Void,
+  onChange: @autoclosure () -> @Sendable () -> Void,
+  isolation: isolated (any Actor)? = #isolation
+) -> ObservationTrackingSubscription {
+  
+  let isCancelled = OSAllocatedUnfairLock<Bool>.init(initialState: false)
+  
+  let _onChange = onChange()
+  
+  _withObservationContinuousTracking(
+    applying: block,
+    isCancelled: isCancelled,
+    onChange: _onChange
+  )
+  
+  return .init {
+    isCancelled.withLock {
+      $0 = true
+    }
+  }
+}
+
+private func _withObservationContinuousTracking(
+  applying block: @escaping () -> Void,
+  isCancelled: OSAllocatedUnfairLock<Bool>,
+  onChange: @escaping @Sendable () -> Void,
+  isolation: isolated (any Actor)? = #isolation
+) {
+  
+  let box = UnsafeSendable(block)
+  
+  do {
+    
+    withObservationTracking {
+      block()      
+    } onChange: {
+      
+      onChange()
+      
+      guard isCancelled.withLock({ !$0 }) else {
+        return
+      }
+      
+      Task {
+        await _withObservationContinuousTracking(
+          applying: box._value,
+          isCancelled: isCancelled,
+          onChange: onChange,
+          isolation: isolation
+        )
+      }
+      
+    }
+  }
+  
+}
+
+private struct UnsafeSendable<V>: ~Copyable, @unchecked Sendable {
+  
+  let _value: V
+  
+  init(_ value: V) {
+    _value = value  
+  }
+  
+}
